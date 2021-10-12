e4eab66af3ae83db7491815edf389f1a4c3cfb78
Caroline Stelitano <carolinestelitano@gmail.com>
2021-10-11 21:31:38 -0300

    Tela Leste Quiz

---
+++ src/main/java/com/grupo1/asreliquiasmatematicas/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.grupo1.asreliquiasmatematicas;
+
+import com.grupo1.asreliquiasmatematicas.ui.TelaLeste;
+
+/**
+ *
+ * @author carol
+ */
+public class Main {
+    public static void main(String[] args) {
+        TelaLeste tela = new TelaLeste();
+        tela.setVisible(true);
+    }
+}
